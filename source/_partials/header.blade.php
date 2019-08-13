e22fd60817687a83f5a48312776d4823a4004d6d
breadthe <omigoshdev@protonmail.com>
2019-08-13 07:37:43 -0500

    Favicons

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,10 +1,10 @@
-<header class="flex items-center h-24 py-4 {{ $page->belongsTo('/blog') ? '' : '' }}" role="banner">
+<header class="flex items-center h-20 sm:h-24 py-4 bg-gray-200 {{ $page->belongsTo('/blog') ? '' : '' }}" role="banner">
     <div class="container flex items-center max-w-4xl mx-auto px-4 lg:px-8">
         <div class="flex items-center">
             <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
-                {{--<img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />--}}
+                <img class="h-12 mr-3" src="/assets/images/chasingcode-logo.png" alt="{{ $page->siteName }} logo" />
 
-                <h1 class="site-logo no-underline text-lg md:text-xl tracking-wide my-0 text-gray-600">
+                <h1 class="no-underline font-normal text-teal-700 text-2xl sm:text-3xl my-0 text-gray-600">
                     {{ $page->siteName }}
                 </h1>
             </a>
