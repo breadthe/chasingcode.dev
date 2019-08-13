d6451d8b05ebe92156c712bd86ef2f7b22c5068e
breadthe <omigoshdev@protonmail.com>
2019-08-13 07:38:23 -0500

    Tweaks

--- source/_nav/menu-toggle.blade.php
+++ source/_nav/menu-toggle.blade.php
@@ -1,4 +1,4 @@
-<button class="flex justify-center items-center border h-10 px-5 rounded-full lg:hidden focus:outline-none {{ $page->belongsTo('/blog') ? '' : '' }}"
+<button class="flex justify-center items-center h-10 px-5 rounded-full lg:hidden focus:outline-none {{ $page->belongsTo('/blog') ? '' : '' }}"
     onclick="navMenu.toggle()"
 >
     <svg id="js-nav-menu-show" xmlns="http://www.w3.org/2000/svg"
