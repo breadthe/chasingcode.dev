0a6a6d86626644f161a0caaaf97ad5513bb54a28
breadthe <omigoshdev@protonmail.com>
2019-08-13 20:39:31 -0500

    Portrait + extract left/right partials

---
+++ source/_partials/left-side.blade.php
@@ -0,0 +1,5 @@
+<div class="w-full sm:w-2/3 p-4 sm:p-16 text-gray-100 text-right">
+    <div class="text-6xl font-thin leading-none">Hello.</div>
+    <div class="text-5xl font-thin leading-none">I'm <span class="font-black text-teal-200">Constantin</span></div>
+    <div class="text-xl font-thin">I'm a web dev and maker in Chicago.</div>
+</div>
---
+++ source/_partials/right-side.blade.php
@@ -0,0 +1,54 @@
+<div class="w-full sm:w-1/3 p-4 sm:p-16 bg-gray-200 text-gray-700">
+    <div class="relative flex items-center justify-center mt-4 sm:mt-0">
+        {{--<img src="assets/images/portrait-bg.svg" class="absolute">--}}
+        {{--<svg viewBox="0 0 20 20" width="20pt" height="20pt">
+            <defs>
+                <clipPath id="_clipPath_VoHTx5yeRPIGAT9ootC4b0BzP8PZ1rJ2"><rect width="20" height="20"/></clipPath>
+            </defs>
+            <g clip-path="url(#_clipPath_VoHTx5yeRPIGAT9ootC4b0BzP8PZ1rJ2)"><path d=" M 17.755 4.178 Q 17.86 5.105 17.231 6.121 C 16.602 7.138 16.648 7.218 16.85 8.236 C 17.052 9.253 20.109 11.087 18.567 12.801 C 17.026 14.515 15.687 16.802 15.439 18.202 Q 15.191 19.601 11.363 19.408 C 10.876 19.383 10.121 19.196 9.679 18.991 L 7.429 17.946 C 5.855 17.242 5.705 16.792 3.462 16.932 C 1.219 17.071 0.798 13.577 1.802 12.095 C 2.805 10.612 1.93 8.994 1.033 7.191 C 0.816 6.754 1.026 6.316 1.503 6.212 Q 4.015 5.666 5.569 4.614 Q 7.124 3.562 8.435 1.72 C 8.718 1.323 9.29 1.198 9.713 1.442 C 9.852 1.522 11.155 3.234 13.371 2.65 Q 15.587 2.066 16.896 2.847 C 17.315 3.097 17.7 3.693 17.755 4.178 Z " fill="rgb(0,0,0)"/></g>
+        </svg>--}}
+        <img src="assets/images/me.jpg" alt="Picture of me" title="Picture of me" class="rounded-full border-8 border-double border-teal-700 w-64 h-64">
+    </div>
+
+    <div class="mt-8 first:mt-0">
+        <div class="text-2xl font-black leading-none text-teal-700 flex flex-col justify-start mb-4">
+            Stats/Bio
+            <hr class="m-0 mt-2 border-b-2 border-gray-400 w-16 text-left">
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Heritage</span> Romanian
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Age</span> I'll buy you a 🍺 if you guess correctly
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Interests</span> Coding, lifting, cycling, skiing, reading. Making things.
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Trivia</span> Lived > 50% of my life outside my country of birth.
+        </div>
+    </div>
+
+    <div class="mt-8 first:mt-0">
+        <div class="text-2xl font-black leading-none text-teal-700 flex flex-col justify-start mb-4">
+            Tech Stack
+            <hr class="m-0 mt-2 border-b-2 border-gray-400 w-16 text-left">
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Laravel</span>
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">Vue.js</span>
+        </div>
+
+        <div class="mb-4">
+            <span class="font-black uppercase">TailwindCSS</span>
+        </div>
+    </div>
+</div>
