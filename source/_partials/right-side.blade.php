6b737220715a245bcf88ebcd235db5b252f8672b
breadthe <omigoshdev@protonmail.com>
2019-08-18 11:38:02 -0500

    Relocate social icons

--- source/_partials/right-side.blade.php
+++ source/_partials/right-side.blade.php
@@ -1,13 +1,16 @@
 <aside class="w-full sm:w-1/3 p-4 sm:p-8 bg-gray-200 text-gray-700">
-    <div class="relative flex items-center justify-center mt-4 sm:mt-0">
-        {{--<img src="assets/images/portrait-bg.svg" class="absolute">--}}
-        {{--<svg viewBox="0 0 20 20" width="20pt" height="20pt">
-            <defs>
-                <clipPath id="_clipPath_VoHTx5yeRPIGAT9ootC4b0BzP8PZ1rJ2"><rect width="20" height="20"/></clipPath>
-            </defs>
-            <g clip-path="url(#_clipPath_VoHTx5yeRPIGAT9ootC4b0BzP8PZ1rJ2)"><path d=" M 17.755 4.178 Q 17.86 5.105 17.231 6.121 C 16.602 7.138 16.648 7.218 16.85 8.236 C 17.052 9.253 20.109 11.087 18.567 12.801 C 17.026 14.515 15.687 16.802 15.439 18.202 Q 15.191 19.601 11.363 19.408 C 10.876 19.383 10.121 19.196 9.679 18.991 L 7.429 17.946 C 5.855 17.242 5.705 16.792 3.462 16.932 C 1.219 17.071 0.798 13.577 1.802 12.095 C 2.805 10.612 1.93 8.994 1.033 7.191 C 0.816 6.754 1.026 6.316 1.503 6.212 Q 4.015 5.666 5.569 4.614 Q 7.124 3.562 8.435 1.72 C 8.718 1.323 9.29 1.198 9.713 1.442 C 9.852 1.522 11.155 3.234 13.371 2.65 Q 15.587 2.066 16.896 2.847 C 17.315 3.097 17.7 3.693 17.755 4.178 Z " fill="rgb(0,0,0)"/></g>
-        </svg>--}}
+    <div class="flex flex-col items-center justify-center mt-4 sm:mt-0">
         <img src="assets/images/me.jpg" alt="Picture of me" title="Picture of me" class="rounded-full border-8 border-double border-teal-700 w-64 h-64">
+
+        <div class="flex mt-8">
+            <a href="https://twitter.com/brbcoding" class="mx-2 text-blue-500 hover:text-blue-700" title="@brbcoding on Twitter">
+                @include('_partials.icons._svg', ['icon' => 'twitter', 'width' => 32, 'height' => 32])
+            </a>
+            <a href="https://github.com/breadthe" class="mx-2 text-green-500 hover:text-green-700" title="@breadthe on Github">
+                @include('_partials.icons._svg', ['icon' => 'github', 'width' => 32, 'height' => 32])
+            </a>
+        </div>
+
     </div>
 
     <div class="mt-8 first:mt-0">
@@ -16,15 +19,6 @@
         </h3>
 
         <hr class="m-0 mb-4 border-b-2 border-gray-400 w-16 text-left">
-
-        <div class="mb-4 flex -mx-2">
-            <a href="https://twitter.com/brbcoding" class="mx-2" title="@brbcoding on Twitter">
-                @include('_partials.icons._svg', ['icon' => 'twitter'])
-            </a>
-            <a href="https://github.com/breadthe" class="mx-2" title="@breadthe on Github">
-                @include('_partials.icons._svg', ['icon' => 'github'])
-            </a>
-        </div>
 
         <dl>
             <div class="mb-4">
