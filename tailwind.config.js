08cd026b970d371f8ad9b829af2dcd3f60d0dc68
breadthe <omigoshdev@protonmail.com>
2019-08-19 21:05:14 -0500

    Add zoom cursor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,48 +1,59 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': [
-          'Raleway',
-          'system-ui',
-          'BlinkMacSystemFont',
-          '-apple-system',
-          'Segoe UI',
-          'Roboto',
-          'Oxygen',
-          'Ubuntu',
-          'Cantarell',
-          'Fira Sans',
-          'Droid Sans',
-          'Helvetica Neue',
-          'sans-serif',
-        ],
-        'serif': [
-          'Bitter',
-          'Constantia',
-          'Lucida Bright',
-          'Lucidabright',
-          'Lucida Serif',
-          'Lucida',
-          'DejaVu Serif',
-          'Bitstream Vera Serif',
-          'Liberation Serif',
-          'Georgia',
-          'serif',
-        ],
-        'mono': [
-          'Menlo',
-          'Monaco',
-          'Consolas',
-          'Liberation Mono',
-          'Courier New',
-          'monospace',
-        ]
-      },
-    }
-  },
-  variants: {
-    margin: ['responsive', 'first'],
-  },
-  plugins: []
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': [
+                    'Raleway',
+                    'system-ui',
+                    'BlinkMacSystemFont',
+                    '-apple-system',
+                    'Segoe UI',
+                    'Roboto',
+                    'Oxygen',
+                    'Ubuntu',
+                    'Cantarell',
+                    'Fira Sans',
+                    'Droid Sans',
+                    'Helvetica Neue',
+                    'sans-serif',
+                ],
+                'serif': [
+                    'Bitter',
+                    'Constantia',
+                    'Lucida Bright',
+                    'Lucidabright',
+                    'Lucida Serif',
+                    'Lucida',
+                    'DejaVu Serif',
+                    'Bitstream Vera Serif',
+                    'Liberation Serif',
+                    'Georgia',
+                    'serif',
+                ],
+                'mono': [
+                    'Menlo',
+                    'Monaco',
+                    'Consolas',
+                    'Liberation Mono',
+                    'Courier New',
+                    'monospace',
+                ]
+            },
+        },
+        cursor: {
+            auto: 'auto',
+            default: 'default',
+            pointer: 'pointer',
+            wait: 'wait',
+            text: 'text',
+            move: 'move',
+            'not-allowed': 'not-allowed',
+            // crosshair: 'crosshair',
+            'zoom-in': 'zoom-in',
+        }
+    },
+    variants: {
+        margin: ['responsive', 'first'],
+    },
+    plugins: []
 }
