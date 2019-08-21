d9ade90f4fbdc84e01373eb99f64df0c8588572b
breadthe <omigoshdev@protonmail.com>
2019-08-20 21:41:10 -0500

    Adjust margin

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="max-w-2xl mx-auto {{ $page->belongsTo('/blog') || $page->belongsTo('/contact') ? 'mt-8' : '' }} mb-8 text-gray-500 italic font-light text-center">
+<footer class="max-w-2xl mx-8 sm:mx-auto {{ $page->belongsTo('/blog') || $page->belongsTo('/contact') ? 'mt-8' : '' }} mb-8 text-gray-500 italic font-light text-center">
     {{ date('Y') }} 🦄 All content is my own and does not reflect my employer's or anyone else's opinions.
 </footer>
