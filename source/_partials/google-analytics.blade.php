55739e36a5e2673feb90ba8dd35acc78309c293d
breadthe <omigoshdev@protonmail.com>
2019-08-21 21:17:44 -0500

    Update GA

--- source/_partials/google-analytics.blade.php
+++ source/_partials/google-analytics.blade.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id="></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1043052-14"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', '');
+    gtag('config', 'UA-1043052-14');
 </script>
