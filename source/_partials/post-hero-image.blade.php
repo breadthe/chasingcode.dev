1d5a3e546d34776449eeb08852c432217938498b
breadthe <omigoshdev@protonmail.com>
2020-03-05 10:03:48 -0600

    Ability to add overlay text to the hero image

--- source/_partials/post-hero-image.blade.php
+++ source/_partials/post-hero-image.blade.php
@@ -1,11 +1,28 @@
 @if($image = $page->image)
-        <section class="w-full flex flex-col items-center">
-                <img src="{{ $image }}" alt="{{ $page->imageAttribution() }}" title="{{ $page->imageAttribution() }}">
+    <section class="w-full flex flex-col items-center justify-center relative">
+        @if($imageOverlayText = $page->image_overlay_text)
+            <div
+                    class="absolute font-black p-12 text-6xl rounded-full"
+                    style="
+                    color: #ff0a5c;
+                    background-color: #ffeb3b;
+                    filter: invert(1);
+                    mix-blend-mode: exclusion;
+                    transform: rotate(-5deg);
+                    box-shadow: 15px 15px #ff0a5c;
+                    text-shadow: 5px 5px 1px #05e2ff;
+                "
+            >
+                    {{ $imageOverlayText }}
+            </div>
+        @endif
 
-                @if($imageAttribution = $page->imageAttribution(true))
-                        <small class="block text-center text-xs">
-                            {!! $imageAttribution !!}
-                        </small>
-                @endif
-        </section>
+        <img src="{{ $image }}" alt="{{ $page->imageAttribution() }}" title="{{ $page->imageAttribution() }}">
+
+        @if($imageAttribution = $page->imageAttribution(true))
+            <small class="block text-center text-xs">
+                {!! $imageAttribution !!}
+            </small>
+        @endif
+    </section>
 @endif
