62f545da825a9a1b143f024876f22cfba5da8968
breadthe <omigoshdev@protonmail.com>
2020-03-27 18:03:05 -0500

    Tweaks

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="max-w-2xl mx-8 sm:mx-auto {{ $page->belongsTo('/blog') || $page->belongsTo('/contact') ? 'mt-8' : '' }} mb-8 text-base text-gray-700 italic font-light text-center">
-    {{ date('Y') }} 🦄 All content is my own and does not reflect my employer's or anyone else's opinions.
+    {{ date('Y') }} 🦄 All content & opinions my own
 </footer>
