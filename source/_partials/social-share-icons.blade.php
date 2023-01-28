23f852166cfe8e331899d7f7a73fb2016fec47f5
breadthe <breadthe@users.noreply.github.com>
2023-01-28 10:24:03 -1000

    Add Mastodon share button

--- source/_partials/social-share-icons.blade.php
+++ source/_partials/social-share-icons.blade.php
@@ -3,6 +3,13 @@
         Liked this article? Share it on your favorite platform.
     </div>
 
+    <a href="javascript:void(0);"
+            id="mastodon-share"
+            data-src="{{ $page->description }}&amp;url={{ $page->getUrl() }}"
+            style="background: #595aff;"
+            class="social--share--icon text-lg text-white hover:text-white rounded font-mono px-2 py-1"
+    >Mastodon</a>
+
     <a href="https://twitter.com/share?url={{ $page->getUrl() }}&text={{ $page->description }}&via={{ $page->twitterHandle }}"
        style="background: #55acee;"
        class="social--share--icon text-lg text-white hover:text-white rounded font-mono px-2 py-1"
@@ -34,3 +41,41 @@
     >Email</a>
 </section>
 
+<script>
+    // @see https://www.bentasker.co.uk/posts/documentation/general/adding-a-share-on-mastodon-button-to-a-website.html
+    doDocumentReady(enableMastodonShare);
+
+    /* Generate a share link for the user's Mastodon domain */
+    function mastodonShare(e) {
+
+        // Get the source text
+        const src = e.target.getAttribute("data-src");
+
+        // Get the Mastodon domain
+        const domain = prompt("Enter your Mastodon domain", "mastodon.social");
+
+        if (domain === "" || domain == null) {
+            return;
+        }
+
+        // Build the URL
+        const url = "https://" + domain + "/share?text=" + src;
+
+        // Open a window on the share page
+        window.open(url, '_blank');
+    }
+
+    function doDocumentReady(fn) {
+        if (document.readyState === "complete" ||
+            (document.readyState !== "loading" && !document.documentElement.doScroll)
+        ) {
+            fn();
+        } else {
+            document.addEventListener("DOMContentLoaded", fn);
+        }
+    }
+
+    function enableMastodonShare() {
+        document.getElementById('mastodon-share').addEventListener('click', mastodonShare);
+    }
+</script>
