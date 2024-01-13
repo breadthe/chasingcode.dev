f5134416e38ef3d83944e01b164f29de2b805390
breadthe <breadthe@users.noreply.github.com>
2024-01-13 09:14:28 -1000

    Global typography -> system fonts

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -24,7 +24,6 @@
             @include('_partials.google-analytics')
         @endif
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,200,300,400,500,600,700,800,900|Bitter" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <link href="https://github.com/breadthe" rel="me">
         <link rel="webmention" href="https://webmention.io/chasingcode.dev/webmention" />
