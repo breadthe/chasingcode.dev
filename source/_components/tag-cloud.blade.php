57d59791ba92a3434abc084152652f1ae9682caa
breadthe <breadthe@users.noreply.github.com>
2024-01-16 21:22:28 -1000

    Redo tag-cloud

--- source/_components/tag-cloud.blade.php
+++ source/_components/tag-cloud.blade.php
@@ -1,21 +1,14 @@
 @props(['categories', 'posts'])
 
-<aside class="sm:flex-1 p-4 sm:p-6 mb-8">
+<aside class="flex flex-col gap-4 sm:flex-1 px-4 mb-8">
     <h2 class="flex items-center justify-between text-teal-700">
         Categories
     </h2>
-    <div class="grid grid-cols-2 sm:grid-cols-1 gap-2">
+    <div class="flex flex-wrap gap-2">
         @foreach ($categories->sortByDesc(function ($category) use ($posts) { return $category->posts($posts)->count(); }) as $tag => $category)
-            <div class="flex items-center text-center">
-                <a
-                        href="{{ '/blog/categories/' . $tag }}"
-                        title="View posts in {{ $tag }}"
-                        class="px-2 border border-dashed rounded bg-teal-100 hover:bg-teal-400 text-teal-700 hover:text-teal-100"
-                >{{ $tag }}</a>
-                <span class="text-base font-light text-right ml-4">
-                            {{ $category->posts($posts)->count() }}
-                        </span>
-            </div>
+            <x-tag href="{{ '/blog/categories/' . $tag }}" title="View posts in {{ $tag }}" count="{{$category->posts($posts)->count()}}">
+                {{ $tag }}
+            </x-tag>
         @endforeach
     </div>
 </aside>
--- source/_components/tag.blade.php
+++ source/_components/tag.blade.php
@@ -1,9 +1,26 @@
-@props(['href', 'title'])
+@props(['href', 'title', 'count' => null])
 
 <a
         href="{{ $href }}"
         title="{{ $title }}"
-        class="category--tag font-mono shadow-inner hover:shadow-md bg-teal-100 hover:bg-teal-400 text-teal-700 hover:text-teal-50 rounded px-2"
+        class="category--tag group inline-flex items-center gap-2 font-mono bg-teal-400 hover:shadow-md rounded"
 >
-    {{ $slot }}
+    <small @class([
+        'rounded-r-none' => $count,
+        'bg-teal-100',
+        'group-hover:bg-teal-400',
+        'text-teal-700',
+        'group-hover:text-white',
+        'px-2',
+        'shadow-inner',
+        'rounded',
+    ])>
+        {{ $slot }}
+    </small>
+
+    @if($count)
+        <small class="font-light text-right pr-2 bg-teal-400 text-white rounded-r">
+            {{ $count }}
+        </small>
+    @endif
 </a>
