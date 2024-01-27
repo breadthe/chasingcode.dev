099699c39bbff77763f18b7f54c0c713c719404f
breadthe <breadthe@users.noreply.github.com>
2024-01-26 22:25:36 -1000

    Projects & Packages pass 1

--- source/_partials/about/open-source.blade.php
+++ source/_partials/about/open-source.blade.php
@@ -1,11 +1,9 @@
 <article class="flex flex-col p-4 sm:p-6 bg-white rounded">
-    <div class="text-center mb-8 sm:mb-4">
-        <h1 class="inline pb-2 border-b-2 border-gray-400 text-gray-900 text-5xl font-sans font-thin">Open Source</h1>
-    </div>
+    <h2>Open Source</h2>
 
-    <div class="mx-auto sm:max-w-lg text-center mb-8 sm:mb-4 text-lg text-teal-700">
+    <p>
         Open source projects free to use by anyone.
-    </div>
+    </p>
 
     <div class="flex flex-col sm:flex-row items-start mt-8">
         <div class="w-full sm:w-1/4 flex justify-center">
@@ -13,9 +11,9 @@
                 <img src="/assets/images/fs-sm.jpg" loading="lazy" width="213" height="163" title="FileServer" alt="FileServer">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">FileServer</span>
+                <strong>FileServer</strong>
             </div>
             <div class="text-black border-black font-light mt-2 border-2 border-dashed p-2 shadow">
                 Free, open source, self-hosted Laravel application for uploading and serving your own files. Supports local disk storage and Backblaze B2 out of the box.
@@ -23,8 +21,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Laravel &bull; Livewire</span>
                 <a href="https://github.com/breadthe/laravel-fileserver" class="inline-flex items-center mt-2 sm:mt-0" title="Source code on Github" target="_blank" rel="noopener">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -38,15 +36,15 @@
                 <img src="/assets/images/git-diff-sm.jpg" loading="lazy" width="213" height="163" title="GitHub Diff" alt="GitHub Diff">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold flex items-center">
+                <span class="font-bold flex items-center">
                     <img src="/assets/images/git-diff-logo.png" class="h-full" width="30" height="30" title="GitHub Diff logo" alt="GitHub Diff logo">&nbsp;
                     GitHub Diff
                 </span>
                 <a href="https://git-diff.netlify.app" class="inline-flex items-center" target="_blank" rel="noopener" title="GitHub Diff on Netlify">
                     git-diff.netlify.app
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="text-black border-black font-light mt-2 border-2 border-dashed p-2 shadow">
@@ -55,8 +53,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Svelte &bull; TailwindCSS &bull; Netlify</span>
                 <a href="https://github.com/breadthe/svelte-git-diff" class="inline-flex items-center mt-2 sm:mt-0" title="Source code on Github" target="_blank" rel="noopener">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -70,15 +68,15 @@
                 <img src="/assets/images/craftnautica-sm.jpg" loading="lazy" width="213" height="163" title="Craftnautica" alt="Craftnautica">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold flex items-center">
+                <span class="font-bold flex items-center">
                     <img src="/assets/images/craftnautica-logo.png" class="h-full" width="30" height="30" title="Craftnautica logo" alt="Craftnautica logo">&nbsp;
                     Craftnautica
                 </span>
                 <a href="https://craftnautica.netlify.app" class="inline-flex items-center" target="_blank" rel="noopener" title="Craftnautica on Netlify">
                     craftnautica.netlify.app
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="bg-blue-900 text-blue-200 border-blue-200 font-light mt-2 border-2 border-dashed p-2 shadow">
@@ -87,15 +85,15 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Svelte &bull; TailwindCSS &bull; Netlify</span>
                 <a href="https://github.com/breadthe/craftnautica2" class="inline-flex items-center mt-2 sm:mt-0" title="Craftnautica in Svelte on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Vue.js &bull; TailwindCSS &bull; Netlify</span>
                 <a href="https://github.com/breadthe/craftnautica" class="inline-flex items-center mt-2 sm:mt-0" title="Craftnautica in Vue 2 on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -109,14 +107,14 @@
                 <img src="/assets/images/tw2-colors-sm.jpg" loading="lazy" width="213" height="163" title="Tailwind 2 Colors" alt="Tailwind 2 Colors">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold flex items-center">
+                <span class="font-bold flex items-center">
                     Tailwind 2 Colors
                 </span>
                 <a href="https://tailwind2-colors.netlify.app" class="inline-flex items-center" target="_blank" rel="noopener" title="Tailwind 2 Colors on Netlify">
                     tailwind2-colors.netlify.app
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="bg-indigo-700 text-pink-100 border-pink-200 font-light mt-2 border-2 border-dashed p-2 shadow">
@@ -125,8 +123,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Svelte &bull; TailwindCSS &bull; Netlify</span>
                 <a href="https://github.com/breadthe/tailwind2-colors" class="inline-flex items-center mt-2 sm:mt-0" title="Tailwind 2 Colors in Svelte on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -140,9 +138,9 @@
                 <img src="/assets/images/folio-sm.jpg" loading="lazy" width="213" height="166" title="Folio - Crypto coin portfolio" alt="Folio - Crypto coin portfolio">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">Folio</span>
+                <strong>Folio</strong>
             </div>
             <div class="bg-gray-900 text-gray-400 border-gray-400 font-light mt-4 border-2 border-dashed p-2 shadow">
                 Crypto coin portfolio and price tracker desktop app.
@@ -150,8 +148,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Vue.js &bull; Electron &bull; TailwindCSS</span>
                 <a href="https://github.com/breadthe/folio" class="inline-flex items-center mt-2 sm:mt-0" title="Folio on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -165,12 +163,12 @@
                 <img src="/assets/images/moviebuff-sm.jpg" loading="lazy" width="213" height="154" title="Moviebuff - Movie watchlist" alt="Moviebuff - Movie watchlist">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">Moviebuff</span>
+                <strong>Moviebuff</strong>
                 <a href="https://moviebuff.surge.sh" class="inline-flex items-center" target="_blank" rel="noopener" title="Moviebuff on Surge.sh">
                     moviebuff.surge.sh
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="bg-yellow-500 text-yellow-900 border-yellow-200 font-light mt-4 border-2 border-dashed p-2 shadow">
@@ -179,8 +177,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Vue.js &bull; Bulma &bull; Surge.sh</span>
                 <a href="https://github.com/breadthe/moviebuff" class="inline-flex items-center mt-2 sm:mt-0" title="Moviebuff on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
@@ -194,9 +192,9 @@
                 <img src="/assets/images/moviebuff-monolith-sm.jpg" loading="lazy" width="213" height="188" title="Moviebuff monolith - Movie watchlist on Laravel" alt="Moviebuff monolith - Movie watchlist on Laravel">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">Moviebuff monolith</span>
+                <strong>Moviebuff monolith</strong>
             </div>
             <div class="bg-yellow-500 text-yellow-900 border-yellow-200 font-light mt-4 border-2 border-dashed p-2 shadow">
                 Similar to the static Vue app above, this version is more advanced, benefiting from a Laravel back-end. It features more complex list management: move and copy movies between existing and new lists.
@@ -204,8 +202,8 @@
             <div class="mt-4 flex flex-col sm:flex-row items-center justify-between">
                 <span>Laravel &bull; Vue.js &bull; Bootstrap</span>
                 <a href="https://github.com/breadthe/moviebuff-monolith" class="inline-flex items-center mt-2 sm:mt-0" title="Moviebuff monolith on Github">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
         </div>
--- source/_partials/about/packages.blade.php
+++ source/_partials/about/packages.blade.php
@@ -1,26 +1,24 @@
 <article class="flex flex-col p-4 sm:p-6 bg-white rounded">
-    <div class="text-center mb-8 sm:mb-4">
-        <h1 class="inline pb-2 border-b-2 border-gray-400 text-gray-900 text-5xl font-sans font-thin">Packages</h1>
-    </div>
+    <h2>Packages</h2>
 
-    <div class="mx-auto sm:max-w-lg text-center mb-8 sm:mb-4 text-lg text-teal-700">
+    <p>
         PHP and Laravel packages to help me with specific tasks.
-    </div>
+    </p>
 
     <div class="flex flex-col sm:flex-row items-start mt-8">
         <div class="w-full sm:w-1/4 flex items-center justify-center text-6xl">
             📦
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">php-contrast</span>
+                <strong>php-contrast</strong>
                 <a href="https://github.com/breadthe/php-contrast" class="inline-flex items-center mt-2 sm:mt-0" title="php-contrast on Github" target="_blank" rel="noopener">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="text-black border-black font-light mt-2 border-2 border-dashed p-2 shadow">
-                <strong class="font-bold">PHP</strong> package for generating accessible/high-contrast color pairs.
+                <strong>PHP</strong> package for generating accessible/high-contrast color pairs.
             </div>
         </div>
     </div>
@@ -33,16 +31,16 @@
                 <img src="/assets/images/php-simple-calendar-sm.jpg" loading="lazy" width="213" height="163" title="php-simple-calendar" alt="php-simple-calendar">
             </a>
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">php-simple-calendar</span>
+                <strong>php-simple-calendar</strong>
                 <a href="https://github.com/breadthe/php-simple-calendar" class="inline-flex items-center mt-2 sm:mt-0" title="php-simple-calendar on Github" target="_blank" rel="noopener">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="text-black border-black font-light mt-2 border-2 border-dashed p-2 shadow">
-                <strong class="font-bold">PHP</strong> package for generating a headless representation of the days in a month, in a 7x6 or 7x5 grid. Useful when building calendars with a month overview.
+                <strong>PHP</strong> package for generating a headless representation of the days in a month, in a 7x6 or 7x5 grid. Useful when building calendars with a month overview.
             </div>
         </div>
     </div>
@@ -53,16 +51,16 @@
         <div class="w-full sm:w-1/4 flex items-center justify-center text-6xl">
             📦
         </div>
-        <div class="w-full sm:w-3/4 ml-2 flex flex-col text-lg">
+        <div class="w-full sm:w-3/4 ml-2 flex flex-col">
             <div class="flex flex-col sm:flex-row items-center justify-between">
-                <span class="text-2xl font-bold">laravel-silent-spam-filter</span>
+                <strong>laravel-silent-spam-filter</strong>
                 <a href="https://github.com/breadthe/laravel-silent-spam-filter" class="inline-flex items-center mt-2 sm:mt-0" title="laravel-silent-spam-filter on Github" target="_blank" rel="noopener">
-                    @include('_partials.icons._svg', ['icon' => 'github']) <span class="ml-1">Github</span>
-                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 20, 'height' => 20])</span>
+                    @include('_partials.icons._svg', ['icon' => 'github', 'width' => 16, 'height' => 16]) <span class="ml-1">Github</span>
+                    <span class="ml-1 text-gray-700">@include('_partials.icons._svg', ['icon' => 'external-link', 'width' => 16, 'height' => 16])</span>
                 </a>
             </div>
             <div class="text-black border-black font-light mt-2 border-2 border-dashed p-2 shadow">
-                <strong class="font-bold">Laravel</strong> package for silently filtering out spam in submitted forms. Add your own words and phrases to the banned list.
+                <strong>Laravel</strong> package for silently filtering out spam in submitted forms. Add your own words and phrases to the banned list.
             </div>
         </div>
     </div>
