e7afffeb02e192b72fe04486b5cca175f0adcac0
breadthe <breadthe@users.noreply.github.com>
2024-01-30 22:35:34 -1000

    Extract hero shadow to variables

--- source/_components/hero.blade.php
+++ source/_components/hero.blade.php
@@ -1,7 +1,7 @@
 @props(['title', 'description' => null])
 
 <section class="hero flex items-center flex-col gap-6 py-8 px-6 sm:px-0">
-        <h1 class="border border-black px-2 uppercase text-white bg-gray-800" style="box-shadow: 6px 6px 0 0 rgba(45,212,191,1)">
+        <h1 class="border border-black px-2 uppercase text-white bg-gray-800" style="box-shadow: var(--hero-title-shadow)">
             {{ $title }}
         </h1>
 
