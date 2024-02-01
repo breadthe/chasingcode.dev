2a6e20888b1e0491c08c4750164c81212c72d368
breadthe <breadthe@users.noreply.github.com>
2024-01-31 22:25:59 -1000

    Redo the Contact form

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -8,48 +8,36 @@
 @endpush
 
 @section('hero')
-    <x-hero title="Contact Me"/>
+    <x-hero title="Contact Me" description="Get in touch" />
 @endsection
 
 @section('body')
 
 <section class="flex flex-col p-4 sm:p-6 bg-white rounded">
-    <h2>Get in touch</h2>
     <p>I'll get back to you when I get to it.</p>
 
     <form
         name="contact"
         method="post"
         data-netlify="true"
-        class="w-full mt-4 bg-gray-200 border shadow"
+        class="w-full flex flex-col items-end gap-4 mt-4 mx-auto max-w-md p-4 bg-gray-100 border rounded"
     >
-        <div
-            class="flex justify-start pb-4"
-        >
-            <div class="w-full">
-                <input class="shadow-inner border" placeholder="Your Name" type="text" id="name" name="name" />
-            </div>
+        <div class="w-full">
+            <input class="w-full rounded p-2 shadow-inner border" placeholder="Your Name" type="text" id="name" name="name" />
         </div>
-        <div
-            class="flex justify-start pb-4"
-        >
-            <div class="w-full">
-                <input class="shadow-inner border" placeholder="Your Email" type="email" id="email" name="email" />
-            </div>
+
+        <div class="w-full">
+            <input class="w-full rounded p-2 shadow-inner border" placeholder="Your Email" type="email" id="email" name="email" />
         </div>
-        <div
-            class="flex justify-start pb-4"
-        >
-            <div class="w-full">
-                <textarea class="shadow-inner border" placeholder="Message" id="message" name="message"></textarea>
-            </div>
+
+        <div class="w-full">
+            <textarea class="w-full rounded p-2 shadow-inner border" placeholder="Message" id="message" name="message"></textarea>
         </div>
-        <div class="text-right">
-            <button
-                type="submit"
-                class=""
-            >Send</button>
-        </div>
+
+        <button
+            type="submit"
+            class="bg-teal-700 text-white px-4 py-2 rounded"
+        >Send</button>
     </form>
 </section>
 
