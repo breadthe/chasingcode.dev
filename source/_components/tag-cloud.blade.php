9841cd1ec39956212642d12b00e650c3cbb25ae8
breadthe <breadthe@users.noreply.github.com>
2024-08-10 14:44:59 -0500

    Fix tag title

--- source/_components/tag-cloud.blade.php
+++ source/_components/tag-cloud.blade.php
@@ -2,7 +2,11 @@
 
 <div class="flex flex-wrap gap-2">
     @foreach ($tags->sortByDesc(function ($tag) use ($posts) { return $tag->posts($posts)->count(); }) as $tag_name => $tag)
-        <x-tag href="/blog/tags/{{ $tag_name }}" title="View posts in {{ $tag_name }}" count="{{$tag->posts($posts)->count()}}" isTagCloud>
+        @php
+            $count = $tag->posts($posts)->count();
+        @endphp
+
+        <x-tag href="/blog/tags/{{ $tag_name }}" title="View {{ $count }} posts {{ \Illuminate\Support\Str::plural('post', $count) }} in #{{ $tag_name }}" count="{{ $count }}" isTagCloud>
             #{{ $tag_name }}
         </x-tag>
     @endforeach
--- source/_components/tag.blade.php
+++ source/_components/tag.blade.php
@@ -1,16 +1,11 @@
 @props(['href', 'title', 'count' => null, 'isTagCloud' => false])
 
-@php
-    use function App\helpers\tagSize;
-    use Illuminate\Support\Str;
-@endphp
-
 <a
         href="{{ $href }}"
-        title="{{ $count }} {{ Str::plural('post', $count) }} in {{ $title }}"
+        title="{{ $title }}"
         class="category--tag font-mono font-light text-teal-700"
         @if($isTagCloud)
-        style="font-size: {{ tagSize($count) }}px;"
+            style="font-size: {{ \App\helpers\tagSize($count) }}px;"
         @endif
 >
     {{ $slot }}
