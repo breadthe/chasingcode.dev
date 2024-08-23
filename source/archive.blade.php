012e1866d7211c7f939b07eeff9f8d317a8b2cf6
breadthe <breadthe@users.noreply.github.com>
2024-08-22 22:52:58 -0500

    Use flex instead of grid for the archive list

--- source/archive.blade.php
+++ source/archive.blade.php
@@ -23,23 +23,21 @@ title: Archive
         <section class="p-4 sm:p-6 bg-white rounded">
             @foreach ($posts->groupBy(function ($post) { return $post->getDate()->format('Y'); }) as $year => $yearPosts)
                 <div class="mb-8">
-                    <h2 class="flex items-center gap-2 mb-2 text-teal-700">
+                    <h2 class="flex items-baseline gap-2 mb-2 text-teal-700">
                         {{ $year }}
                         <small class="text-xs text-gray-400 font-light">({{ $yearPosts->count() }})</small>
                     </h2>
 
                     @foreach ($yearPosts as $post)
-                        <div class="grid grid-cols-6 mb-2 items-center">
-                            <span class="col-span-1 text-xs text-gray-400 font-mono">
+                        <div class="flex items-baseline mb-2">
+                            <span class="w-16 text-xs text-gray-400 font-mono">
                                 {{ $post->getDate()->format('M d') }}
                             </span>
 
-                            <span class="col-span-5">
-                                <a
-                                        href="{{ $post->getUrl() }}"
-                                        class="underline underline-offset-2 decoration-sky-200 hover:decoration-sky-400 decoration-2"
-                                >{{ $post->title }}</a>
-                            </span>
+                            <a
+                                    href="{{ $post->getUrl() }}"
+                                    class="flex-1 underline underline-offset-2 decoration-teal-200 hover:decoration-teal-400 decoration-2"
+                            >{{ $post->title }}</a>
                         </div>
                     @endforeach
                 </div>
