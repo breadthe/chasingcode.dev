3bc4a0b991f633d7a23bbfb88887c2d28f20ea99
breadthe <breadthe@users.noreply.github.com>
2026-05-18 23:28:47 -0500

    Fix Tailwind 4 migration

--- source/_components/tag.blade.php
+++ source/_components/tag.blade.php
@@ -3,7 +3,7 @@
 <a
         href="{{ $href }}"
         title="{{ $title }}"
-        class="category--tag font-mono font-light text-teal-700"
+        class="category--tag font-mono font-light text-teal-700 hover:text-black"
         @if($isTagCloud)
             style="font-size: {{ \App\helpers\tagSize($count) }}px;"
         @endif
